987162e25c47c681ca6381f6a96ce9f1094911eb
cameramanbrayton <info@duelingworlds.com>
2021-04-14 01:40:05 -0500

    shopping-@-Vivians-Quick-App02

--- app/src/main/java/com/vivsquickapp/shoppingviviansquickapp/MainActivity.java
+++ app/src/main/java/com/vivsquickapp/shoppingviviansquickapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.vivsquickapp.shoppingviviansquickapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.vivsHomeTextView);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
+
+    public void openBrowser(View view){
+
+        //Get url from tag
+        String url = (String)view.getTag();
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        //pass the url to intent data
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
+    }
+
 }
