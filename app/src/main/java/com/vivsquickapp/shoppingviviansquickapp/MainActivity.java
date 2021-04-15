de7aa73623a82c78aa0cc3e0e1fced03bb5565b7
cameramanbrayton <info@duelingworlds.com>
2021-04-15 03:03:28 -0500

    shopping-@-Vivians-Quick-App11

--- app/src/main/java/com/vivsquickapp/shoppingviviansquickapp/MainActivity.java
+++ app/src/main/java/com/vivsquickapp/shoppingviviansquickapp/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.vivsBlogTextView);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        textView = findViewById(R.id.vivsContactTextView);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void openBrowser(View view){
